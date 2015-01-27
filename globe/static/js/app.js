7987604f0d6ff3a6c7f66a3fb510e3a88160aae1
Dan Petker <dpetker@users.noreply.github.com>
2015-01-26 20:19:59 -0500

    Sample data is now polled from server. Still have performance issues, though...

--- globe/static/js/app.js
+++ globe/static/js/app.js
@@ -1,40 +1,31 @@
 $(function() {
-
-  var _sampleData, _animateIntervalId, _globe;
-
   if( !Detector.webgl ) {
     Detector.addGetWebGLMessage();
   } else {
     var container = document.getElementById( 'container' );
-    _globe = new DAT.Globe( container );
+    var globe = new DAT.Globe( container );
 
     TWEEN.start();
 
-    $.ajax( 'sample-data.json' )
-      .done( function( data ) {
-        _sampleData = data;
-        _animateIntervalId = window.setInterval( animateData, 1000 );
-        document.body.style.backgroundImage = 'none'; // remove loading
-      })
-      .fail( function( request, status ) {
-        alert( 'Failed to load data, reason: ' + status );
-      });
+    window.setInterval( function() {
+      $.ajax( '/data' )
+        .done( function( data ) {
+          if( data.length == 0 ) {
+            return;
+          }
+
+          globe.addData( data, {
+            format: 'magnitude',
+            name: 'Sample Data'
+          });
+          globe.createPoints();
+          globe.animate();
+
+          document.body.style.backgroundImage = 'none'; // remove loading
+        })
+        .fail( function( request, status ) {
+          alert( 'Failed to load data, reason: ' + status );
+        });
+    }, 1000 );
   }
-
-  var animateData = function() {
-    if( _sampleData.length == 0 ) {
-      window.clearInterval( _animateIntervalId );
-      return;
-    }
-
-    // Add 10 points at a time
-    var nextTen = _sampleData.splice( 0, 30 );
-    _globe.addData( nextTen, {
-      format: 'magnitude',
-      name: 'Sample Data'
-    });
-
-    _globe.createPoints();
-    _globe.animate();
-  };
 });
--- index.js
+++ index.js
@@ -4,10 +4,35 @@ Index.js - Entry point to serve up the WebGL Globe
 
 var express = require( 'express' );
 var serveStatic = require( 'serve-static' );
+var fs = require( 'fs' );
+
+var _sampleData = JSON.parse( fs.readFileSync( './globe/sample-data.json' ) );
 
 var app = express();
 
+// For CORS
+app.use( function( req, res, next ) {
+  res.header( "Access-Control-Allow-Origin", "*" );
+  res.header( "Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept" );
+  res.header( "Access-Control-Allow-Methods", "GET" );
+
+  if( req.method == 'OPTIONS' ) {
+    res.json( [] );
+  } else {
+    next();
+  }
+});
+
 app.use( serveStatic( 'globe', { 'index': ['index.html', 'index.htm' ] } ) );
+
+app.get( '/data', function( req, res ) {
+  if( _sampleData.length == 0 ) {
+    return res.json( [] );
+  }
+
+  res.json( _sampleData.splice( 0, 30 ) );
+});
+
 app.listen( 8080 );
 
 console.log( 'Listening on port 8080...' );
