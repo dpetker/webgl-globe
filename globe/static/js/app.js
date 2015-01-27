5b6b9ca3b1c510c381b1c1d8fe1f9c71955b604b
Dan Petker <dpetker@users.noreply.github.com>
2015-01-26 19:36:46 -0500

    Yet more code cleanup.

--- globe/static/js/app.js
+++ globe/static/js/app.js
@@ -2,30 +2,25 @@ $(function() {
   if( !Detector.webgl ) {
     Detector.addGetWebGLMessage();
   } else {
-
-    var years = ['1990','1995','2000'];
     var container = document.getElementById( 'container' );
     var globe = new DAT.Globe( container );
 
-    console.log( globe );
-    var i, tweens = [];
-
     var settime = function( globe, t ) {
       return function() {
-        new TWEEN.Tween( globe ).to( { time: t/years.length }, 500 ).easing( TWEEN.Easing.Cubic.EaseOut ).start();
+        new TWEEN.Tween( globe ).to( { time: t }, 500 ).easing( TWEEN.Easing.Cubic.EaseOut ).start();
       };
     };
 
     TWEEN.start();
 
-    $.ajax( 'population909500.json' )
+    $.ajax( 'sample-data.json' )
       .done( function( data ) {
         window.data = data;
-        for (i=0;i<data.length;i++) {
-          globe.addData(data[i][1], {format: 'magnitude', name: data[i][0], animated: true});
+        for( var i = 0; i < data.length; i++ ) {
+          globe.addData( data[i][1], { format: 'magnitude', name: data[i][0], animated: true } );
         }
         globe.createPoints();
-        settime(globe,0)();
+        settime( globe, 0 )();
         globe.animate();
         document.body.style.backgroundImage = 'none'; // remove loading
       })
